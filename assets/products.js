4e214a29f35003a04a75f44830b8071a54d46edb
11ONGOAL <vasiliualexandru555@yahoo.com>
2026-09-10 12:07:06 +0300

    Update products.js

--- assets/products.js
+++ assets/products.js
@@ -704,7 +704,7 @@ window.PRODUCTS = [
     "stock": "1000",
     "video": "",
     "category": "Casă și grădină",
-    "image": "assets/images/patura-impermeabila.webp",
+    "image": "assets/images/fdggfh (1).png",
     "featured": false
   },
   {
