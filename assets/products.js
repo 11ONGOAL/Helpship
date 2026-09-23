38a40521741ac1949094b48fa4d921cab309d142
11ONGOAL <vasiliualexandru555@yahoo.com>
2026-09-23 10:43:00 +0300

    Add new product and remove duplicate entry
    Added a new product 'Ultra Adeziv Anti-Scurgeri StrongFix' to the PRODUCTS array and removed the duplicate entry.

--- assets/products.js
+++ assets/products.js
@@ -1,4 +1,16 @@
 window.PRODUCTS = [
+    {
+    "id": 65,
+    "slug": "ultra-adeziv-anti-scurgeri-strongfix",
+    "name": "Ultra Adeziv Anti-Scurgeri StrongFix",
+    "description": "Confort și îngrijire zilnică",
+    "price": "0 Lei",
+    "stock": "0 Buc.",
+    "video": "",
+    "category": "Casă și Grădină",
+    "image": "assets/images/Media1_dc57a0f9-af5b-4c76-913b-06cd214261d1.webp",
+    "featured": false
+  },
   {
     "id": 61,
     "slug": "patura-impermeabila",
@@ -729,18 +741,6 @@ window.PRODUCTS = [
     "video": "",
     "category": "Beauty",
     "image": "assets/images/crema-pentru-hemoroizi-20g.webp",
-    "featured": false
-  },
-  {
-    "id": 65,
-    "slug": "ultra-adeziv-anti-scurgeri-strongfix",
-    "name": "Ultra Adeziv Anti-Scurgeri StrongFix",
-    "description": "Confort și îngrijire zilnică",
-    "price": "0 Lei",
-    "stock": "0 Buc.",
-    "video": "",
-    "category": "Casă &amp; Grădină",
-    "image": "assets/images/Media1_dc57a0f9-af5b-4c76-913b-06cd214261d1.webp",
     "featured": false
   },
 ];
